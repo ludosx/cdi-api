c63368f1951f1efa974377c50b08aa61ad0ee254
Your Name <you@examaple.com>
2023-07-11 20:44:16 -0500

    initial

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from database import Base 
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100))
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title= Column(String(100))
+    content = Column(String(1000))
+    user_id = Column(Integer)
+
+    
